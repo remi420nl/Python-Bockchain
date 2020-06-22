1a2a34a602c69a208e23161712bf631df518d95a
Remi Peerlings <remi.p@hotmail.com>
2020-06-22 16:37:21 +0200

    up to proof of work method

--- BLC1/BLC1.py
+++ BLC1/BLC1.py
@@ -1,12 +1,34 @@
 
+import hashlib
+import json
+from time import time
+from uuid import uuid4
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-       # Creates a new Block and adds it to the chain
-        pass
+        # Create genesis block
+
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+         """
+        Create a new Block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+
+         block = {
+         'index' : len(self.chain) +1,
+         'timestamp' : time(),
+         'transactions': self.current_transactions,
+         'proof': proof,
+         'previous_hash' : previous_hash or self.hash(self.chain[-1])
+         }
 
     def new_transaction(self, sender, recipient, amount):
         """
@@ -28,9 +50,37 @@ class Blockchain(object):
     @staticmethod
     def hash(block):
         # Hashes a Block
-        pass
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string)
 
     @property
     def last_block(self):
-        # Returns the last Block in the chain
-        pass
+        return self.chain[-1]
+
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+            return proof
+    staticmethod
+    def valid_proof(last_proof, proof):
+
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
